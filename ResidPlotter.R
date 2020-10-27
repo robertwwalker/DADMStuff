df14f06dae6867bf9894e0eecf946d71922feba9
Robert W. Walker <rwalker@willamette.edu>
2020-10-27 14:05:08 -0700

    Add files via upload

---
+++ ResidPlotter.R
@@ -0,0 +1,15 @@
+library(tidyverse); library(patchwork)
+resid.plotter <- function(model) {
+  r1 <- residuals(model)
+  fv = fitted.values(model)
+  sv <- paste0("Shapiro p-value: ",round(shapiro.test(Model1$residuals)$p.value, 5))
+  rcall <- deparse(model$call)
+  data <- data.frame(Residuals = r1, Fitted.Values = fv) %>% mutate(y = Residuals + Fitted.Values)
+  plot1 <-  data %>% ggplot() + aes(x=Residuals) + geom_density()
+  plot2 <-  data %>% ggplot() + aes(x=Fitted.Values, y=y) + geom_point() + geom_abline(intercept=0, slope=1, color="blue")
+  plot3 <-  data %>% ggplot() + aes(x=Fitted.Values, y=Residuals) + geom_point()
+  plot4 <-  data %>% ggplot() + aes(sample=Residuals) + stat_qq() + stat_qq_line() + labs(title = sv)
+  retplot <- (plot1 + plot2) / (plot4 + plot3)
+  retplot <- retplot + plot_annotation(title=rcall)
+ return(retplot)
+}
